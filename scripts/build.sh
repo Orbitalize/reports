384b08fb180b129d73237dc182da82ab27a20e41
Michael Barroco <michael@orbitalize.com>
2023-08-14 10:00:27 +0200

    [ci] Create github action release (#4)

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Find and change to repo root directory
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+	# OSX uses BSD readlink
+	BASEDIR="$(dirname "$0")"
+else
+	BASEDIR=$(readlink -e "$(dirname "$0")")
+fi
+cd "${BASEDIR}/.." || exit 1
+
+cd tools/
+yarn
+yarn run generate-types
+
+cd ../reports
+yarn
+yarn run build
+
+cd dist/
+zip -r app.zip .
