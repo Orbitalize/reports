f55599d285597cbec3d62a5a0b5845e538e93420
Christopher Chiche <christopher.chiche@orbitalize.com>
2023-08-23 16:48:45 +0200

    Cleanup css

--- reports/src/main.tsx
+++ reports/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ErrorBoundary } from "./utils/ErrorBoundary.tsx";
+import { CssBaseline } from "@mui/material";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -12,6 +13,7 @@ import "@fontsource/roboto/700.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
+      <CssBaseline />
       <App />
     </ErrorBoundary>
   </React.StrictMode>
