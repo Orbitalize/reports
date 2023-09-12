971cc80d131875eacc1b31905644147685a0a772
Christopher Chiche <christopher.chiche@orbitalize.com>
2023-09-12 15:16:56 +0200

    App layout (theme, dark mode, app bar) (#21)
    Light mode: 
    <img width="1510" alt="Screenshot 2023-08-28 at 12 03 22"
    src="https://github.com/Orbitalize/reports/assets/1410314/71211e19-7e78-45c6-8dbf-b285880c0fcf">
    
    Dark mode: 
    <img width="1511" alt="Screenshot 2023-08-28 at 12 03 39"
    src="https://github.com/Orbitalize/reports/assets/1410314/fce5312d-9ca0-48d2-ba3c-97869ffceca1">

--- reports/src/main.tsx
+++ reports/src/main.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { ErrorBoundary } from "./utils/ErrorBoundary.tsx";
-import { CssBaseline } from "@mui/material";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -13,7 +11,6 @@ import "@fontsource/roboto/700.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <CssBaseline />
       <App />
     </ErrorBoundary>
   </React.StrictMode>
