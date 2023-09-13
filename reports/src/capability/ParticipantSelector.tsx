f71a501a46d970d967439ee9ba9275e36bf16e55
Christopher Chiche <christopher.chiche@orbitalize.com>
2023-09-13 13:50:06 +0200

    Cleanup participant selector and dark mode (#29)

--- reports/src/capability/ParticipantSelector.tsx
+++ reports/src/capability/ParticipantSelector.tsx
@@ -17,27 +17,27 @@ export const ParticipantSelector = ({
 }) => {
   const location = useLocation();
   const navigate = useNavigate();
-  const ussp = parseInt(location.pathname.split("/")[1]);
+  const participantId = parseInt(location.pathname.split("/")[1]);
 
-  const handleUsspSelect = (event: SelectChangeEvent<number>) => {
+  const handleParticipantSelect = (event: SelectChangeEvent<number>) => {
     const id = event.target.value;
     navigate(`/${id}`);
   };
-  if (hideIfNoParticipant && Number.isNaN(ussp)) {
+  if (hideIfNoParticipant && Number.isNaN(participantId)) {
     return null;
   }
   return (
     <FormControl size="small">
-      <InputLabel id="ussp-select-label">USSP</InputLabel>
+      <InputLabel id="ussp-select-label">Participant</InputLabel>
       <Select
         displayEmpty={true}
-        label="USSP"
-        value={ussp}
-        onChange={handleUsspSelect}
-        labelId="ussp-select-label"
+        label="Participant"
+        value={participantId}
+        onChange={handleParticipantSelect}
+        sx={{ minWidth: 90 }}
       >
         {report.participants?.map((p, i) => (
-          <MenuItem value={i} key={i}>
+          <MenuItem value={i} key={i} sx={{ minWidth: 30 }}>
             {p}
           </MenuItem>
         ))}
